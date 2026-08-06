7038fab78bb51a44f2cdf1f158cdcb31e9faf00d
Tony <ramirua.fm@gmail.com>
2026-08-06 19:03:54 +0300

    fix: load portable attachment link path encoder

--- extension/background-entry.js
+++ extension/background-entry.js
@@ -87,3 +87,4 @@ importScripts('attachment-output-index-fix.js');
 importScripts('attachment-output-card-geometric-bridge.js');
 importScripts('attachment-output-index-bridge-cleanup.js');
 importScripts('attachment-filename-preservation-fix.js');
+importScripts('attachment-link-path-fix.js');
